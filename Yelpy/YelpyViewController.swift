2c6a7d2b801888ba046508ad4d8069536a161ec9
kavitagaitonde <kasathe@yahoo.com>
2017-09-19 10:34:00 -0700

    First checkin

---
+++ Yelpy/YelpyViewController.swift
@@ -0,0 +1,50 @@
+//
+//  YelpyViewController.swift
+//  Yelpy
+//
+//  Created by Kavita Gaitonde on 9/18/17.
+//  Copyright © 2017 Kavita Gaitonde. All rights reserved.
+//
+
+import UIKit
+
+class YelpyViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // MARK: - Table view
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
